ba9366bb045b040c5c95c9929559a166d8d59e71
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-10-14 00:00:45 +0530

    feat: create func that greets user

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+// Hello returns a greeting for the named person.
+func Hello(name string) string {
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message
+}
+
 func main() {
-	fmt.Println("Namaskaram World")
+	fmt.Println(Hello("Yunus"))
 }
