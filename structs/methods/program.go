b72c6595824d8c9821a52a08ea6296cf245dee94
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-10-15 21:20:41 +0530

    feat(struct): try struct methods

---
+++ structs/methods/program.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type authenticationInfo struct {
+	username string
+	password string
+}
+
+// ?
+func (authInfo authenticationInfo) getBasicAuth() string {
+	return fmt.Sprintf("Authorization: Basic %s:%s", authInfo.username, authInfo.password)
+}
+
+// don't touch below this line
+
+func test(authInfo authenticationInfo) {
+	fmt.Println(authInfo.getBasicAuth())
+	fmt.Println("====================================")
+}
+
+func main() {
+	test(authenticationInfo{
+		username: "Google",
+		password: "12345",
+	})
+	test(authenticationInfo{
+		username: "Bing",
+		password: "98765",
+	})
+	test(authenticationInfo{
+		username: "DDG",
+		password: "76921",
+	})
+}
