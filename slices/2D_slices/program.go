db1b4235dc8f48bdd06fe70d70a4a26cb058e32c
Yunus Parvez Khan <developer.ypk@gmail.com>
2023-11-07 23:18:50 +0530

    feat(slices): create a 2D matrix table print using nested slices

---
+++ slices/2D_slices/program.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func createMatrix(rows, cols int) [][]int {
+	matrix := make([][]int, 0)
+
+	for i := 0; i < rows; i++ {
+		row := make([]int, 0)
+		for j := 0; j < cols; j++ {
+			row = append(row, i*j)
+		}
+		matrix = append(matrix, row)
+	}
+
+	return matrix
+}
+
+// dont edit below this line
+
+func test(rows, cols int) {
+	fmt.Printf("Creating %v x %v matrix...\n", rows, cols)
+	matrix := createMatrix(rows, cols)
+	for i := 0; i < len(matrix); i++ {
+		fmt.Println(matrix[i])
+	}
+	fmt.Println("===== END REPORT =====")
+}
+
+func main() {
+	test(3, 3)
+	test(5, 5)
+	test(10, 10)
+	test(15, 15)
+}
